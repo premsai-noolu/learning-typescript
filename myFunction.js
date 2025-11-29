386d16fe0768fa2cc97aa8d13d0b0a7d54c096e6
premsai-noolu <premsainoolu01@gmail.com>
2025-11-29 14:52:28 +0530

    working with function in ts

---
+++ myFunction.js
@@ -0,0 +1,6 @@
+function signUpUser(name, email, isPaid) { }
+signUpUser("prem", "prem@gmail.com", false);
+var loggedUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+loggedUser("prem", "prem@gmail.com");
---
+++ myFunction.ts
@@ -0,0 +1,7 @@
+function signUpUser(name: string, email: string, isPaid: boolean) {}
+
+signUpUser("prem", "prem@gmail.com", false);
+
+let loggedUser = (name: string, email: string, isPaid: boolean = false) => {};
+
+loggedUser("prem", "prem@gmail.com");
