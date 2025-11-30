4f2a3dbb556ffe08677aa40e53509eb6fae72388
premsai-noolu <premsainoolu01@gmail.com>
2025-11-30 13:10:45 +0530

    Interfaces in Ts

---
+++ myInterfaces.js
@@ -0,0 +1,15 @@
+var hitesh = {
+    dbId: 22,
+    email: "h@h.com",
+    userId: 2211,
+    role: "admin",
+    githubToken: "github",
+    startTrail: function () {
+        return "trail started";
+    },
+    getCoupon: function (name, off) {
+        return 10;
+    },
+};
+hitesh.email = "h@hc.com";
+// hitesh.dbId = 33
---
+++ mytuples.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// const user: (string | number)[] = [1, "hc"]
+var tUser;
+tUser = ["hc", 131, true];
+var rgb = [255, 123, 112];
+var newUser = [112, "example@google.com"];
+newUser[1] = "hc.com";
