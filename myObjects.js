c9252445b6dc2e25bf2c5253c21526d777117820
premsai-noolu <premsainoolu01@gmail.com>
2025-11-30 09:54:44 +0530

    Objects in ts

--- myObjects.js
+++ myObjects.js
@@ -1,4 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 function createUser(user) {
     return { name: "prem", email: "prem@gmail.com", isActive: false };
 }
 createUser({ name: "prem", email: "prem@gmail.com", isActive: false });
+var newUser = {
+    _id: 12,
+    name: "prem",
+    email: "prem@gmail.com",
+    isActive: false,
+};
+newUser.email = "anil@gmail.com";
+function cardInfo(Details) { }
+cardInfo({ cardNumber: 123, cardDate: "240811", cvv: 2345 });
--- myObjects.ts
+++ myObjects.ts
@@ -17,7 +17,7 @@ type User1 = {
   name: string;
   email: string;
   isActive: boolean;
-  creditCardDetails?: number;
+  creditCardDetails?: number; // This feild is optional means user can have this field while creating or not.
 };
 
 let newUser: User1 = {
@@ -29,4 +29,20 @@ let newUser: User1 = {
 
 newUser.email = "anil@gmail.com";
 //newUser._id=13 // This will throw an error cannot assign again to _is because it is readonly.
+
+type cardNumber = {
+  cardNumber: number;
+};
+
+type cardDate = {
+  cardDate: string;
+};
+
+type cardDetails = cardNumber &
+  cardDate & {
+    cvv: number;
+  };
+
+function cardInfo(Details: cardDetails) {}
+cardInfo({ cardNumber: 123, cardDate: "240811", cvv: 2345 });
 export {};
