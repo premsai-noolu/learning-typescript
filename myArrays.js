bb62352515bda4a86825919ba57b48c03134038f
premsai-noolu <premsainoolu01@gmail.com>
2025-11-30 10:49:09 +0530

    Arrays in Ts

---
+++ myArrays.js
@@ -0,0 +1,7 @@
+var superheroes = [];
+//const heroPower: number[] =[]
+var heroPower = [];
+superheroes.push("spiderman");
+heroPower.push(3);
+var allUsers = [];
+allUsers.push({ name: "prem", isActive: true });
---
+++ myArrays.ts
@@ -0,0 +1,15 @@
+const superheroes: string[] = [];
+
+//const heroPower: number[] =[]
+const heroPower: Array<number> = [];
+
+superheroes.push("spiderman");
+heroPower.push(3);
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUsers: User[] = [];
+allUsers.push({ name: "prem", isActive: true });
